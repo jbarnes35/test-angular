0193c22e9b7bdf81b31c06c0806db88c843acdf0
pongpapat <pongpapat63@gmail.com>
2020-05-13 16:07:12 +0700

    จาวาสคิป

--- src/app/components/foot/foot.component.ts
+++ src/app/components/foot/foot.component.ts
@@ -10,6 +10,45 @@ export class FootComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $ (document).ready (function () {
+      var menu = $ ('.menu');
+      var hamburger = $ ('.hamburger');
+      var line = $ ('.line');
+      var menuopen;
+    
+      function openMenu () {
+        menu.css ('left', '0px');
+        line.css ('background', '#fff');
+        menuopen = true;
+      }
+      function closeMenu () {
+        menu.css ('left', '-320px');
+        line.css ('background', '#bcad90');
+        menuopen = false;
+      }
+      function toggleMenu () {
+        if (menuopen) {
+          closeMenu ();
+        } else {
+          openMenu ();
+        }
+      }
+      hamburger.on ({
+        mouseenter: function () {
+          openMenu ();
+        },
+      });
+      menu.on ({
+        mouseleave: function () {
+          closeMenu ();
+        },
+      });
+      hamburger.on ({
+        click: function () {
+          toggleMenu ();
+        },
+      });
+    });
   }
 
 }
